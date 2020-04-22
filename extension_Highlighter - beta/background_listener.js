7c72861d36be1658f50093b5f9aa7167b2020e90
VBS-you <inspirehep@gmail.com>
2020-04-22 16:06:04 +0800

    ver 0.3.6
    mine sweeping 
    auto jump to next content

--- extension_Highlighter - beta/background_listener.js
+++ extension_Highlighter - beta/background_listener.js
@@ -5,23 +5,29 @@
 chrome.browserAction.onClicked.addListener(
      (tabs) =>{
 
-        chrome.tabs.query({
-            "active": true,
-            "currentWindow": true
-        },
-        function(tabs) {
-            if ("undefined" != typeof tabs[0].id && tabs[0].id) {
-
-                chrome.tabs.sendMessage(tabs[0].id, {
-                    "action":"painting"
-
-                });
-                
-            }
-        }
-    );
+        sendmesg()
 
     })
 
 
 
+
+    
+
+function sendmesg(message) {
+    chrome.tabs.query({
+        "active": true,
+        "currentWindow": true
+    },
+    function(tabs) {
+        if ("undefined" != typeof tabs[0].id && tabs[0].id) {
+           
+            chrome.tabs.sendMessage(tabs[0].id, {
+                "action":"painting"
+
+            });
+            
+        }
+    }
+);
+}
